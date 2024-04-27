23045ed68f2d57e35856aae9d90c55355a62c6f7
Aryan Bhati <aryanbhati9638@gmail.com>
2024-04-27 18:59:54 +0530

    pass str

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [length, setLength] = useState(8);
+  const [numAllowed, setNumAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("assa");
+
+  const passGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRKSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (numAllowed) str += "0123456789";
+    if (charAllowed) str += "@-*&%#~+/.";
+
+    for (let i = 0; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass = str.charAt(char);
+    }
+    setPassword(pass);
+  }, [length, numAllowed, charAllowed, setPassword]);
 
   return (
-    <>
-      <div className="bg-red-600 w-screen h-screen "></div>
-    </>
+    <div className="w-screen h-screen flex flex-col justify-center items-center bg-[#212121]">
+      <h1>Password Generator</h1>
+      <div className="w-full t "></div>
+    </div>
   );
 }
 
